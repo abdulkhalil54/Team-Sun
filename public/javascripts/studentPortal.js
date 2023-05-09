6f3bc2632561cd3482091f81da0188ae04fda0e7
jinc0930 <jinc0930@gmail.com>
2023-05-09 10:44:36 -0400

    newApplication button on student portalshould now redirect

--- public/javascripts/studentPortal.js
+++ public/javascripts/studentPortal.js
@@ -20,3 +20,26 @@ function signOutFunc(event){
         console.error('Error:', error);
     });
 }
+
+function newApplicationButton(event){
+    event.preventDefault();
+    fetch('/application', {
+        method: 'GET',
+    })
+    .then(res => {
+        console.log(res);
+        const errorMessageElement = document.querySelector('#errorMessage')
+        if(res.redirected) {
+            console.log("SHOULD NOT HAPPEN");
+            window.location.replace(res.url);
+        }
+        if (!res.ok) {
+            res.json().then(result =>{
+                errorMessageElement.innerHTML = result.message
+            })
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
