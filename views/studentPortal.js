dfa2704f8394536c535ab5e0671aea10818fc88a
jinc0930 <jinc0930@gmail.com>
2023-05-16 12:06:06 -0400

    spacee, quick update before rebranch

--- views/studentPortal.js
+++ views/studentPortal.js
@@ -2,11 +2,13 @@ const studentName = document.getElementById("student-name");
 async function render(){
     const res = await fetch("testData.json");
     if(res.ok){
+        //student Name field
         const data = await res.json();
         console.log(data);
         studentName.innerText = `${data.firstName} ${data.lastName}`;
 
         //order section by preference
+        
     }
     else{
         console.log(`Error: ${res.status}`);
