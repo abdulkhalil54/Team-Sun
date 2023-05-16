a7c3072d13718d09f286d5cf9dce64f860ec9a02
ChilliCreek <cerdenebileg@umass.edu>
2023-05-16 13:46:00 -0400

    Reoganized databse management, added endpoints #31, 32, 33

---
+++ database_administration/databaseManager.py
@@ -0,0 +1,65 @@
+import psycopg2
+
+class databaseManager:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            database="postgres",
+            user="admin_sun",
+            password="teamsun429",
+            host="team-sum-database.cpys0k4l69be.us-east-1.rds.amazonaws.com",
+            port='5432'
+        )
+        self.cursor = self.conn.cursor()
+        self.conn.autocommit = True
+        
+        #SQL query strings to create tables:
+        self.createApplicationsQuery = "CREATE TABLE applicationInfo(username varchar(50), name varchar(100), year varchar(10), grade char, \
+            referenceName varchar(100), referenceContact varchar(100), attachment varchar(200), preferences integer[4][2], matched int);"
+        self.createSectionsQuery = "CREATE TABLE Sections(id int, day varchar(10), time varchar(30), username varchar(50), applicant varchar(50), capacity int, numEnrolled int)"
+        self.createProfessorsQuery = "CREATE TABLE Professors(username varchar(50), section int, ranking int, appRanked varchar(50))"
+        self.createUsersQuery = "CREATE TABLE Users(username varchar(50) primary key, email varchar(100), password varchar(255), type Boolean, firstName varchar(50), lastName varchar(50))"
+        self.createMatchingsQuery = "CREATE TABLE Matchings(section int, username varchar(50))"
+    # ------------------------------------------------------------------------------------------------------
+    def listTables(self):
+        self.cursor.execute("""SELECT * FROM information_schema.tables
+            WHERE table_schema = 'public'""")
+        for table in self.cursor.fetchall():
+            print(table)
+    # ------------------------------------------------------------------------------------------------------
+    def deleteTables(self):
+        self.cursor.execute("DROP TABLE applicationInfo")
+        self.cursor.execute("DROP TABLE Sections")
+        self.cursor.execute("DROP TABLE Professors")
+        self.cursor.execute("DROP TABLE Users")
+        self.cursor.execute("DROP TABLE Matchings")
+    # ------------------------------------------------------------------------------------------------------
+    def createTables(self):
+        self.cursor.execute(self.createApplicationsQuery)
+        self.cursor.execute(self.createSectionsQuery)
+        self.cursor.execute(self.createProfessorsQuery)
+        self.cursor.execute(self.createUsersQuery)
+        self.cursor.execute(self.createMatchingsQuery)
+    # ------------------------------------------------------------------------------------------------------
+    def listRows(self, tableName):
+        self.cursor.execute(f"SELECT * FROM {tableName}")
+        print(f"Contents of table {tableName} are:")
+        for row in self.cursor.fetchall():
+            print(row)  
+
+    # fill dummy values into _test tables 
+    # the dummy values are rough / inconsistent at the moment... simply a testing area
+    def fill_dummy_values(self):
+        #Applications_test: 
+        self.cursor.execute("INSERT INTO Applications_test(username, firstName, lastName, year, gpa, grade320, reference, attachment) VALUES ('testusername', 'testFirst', 'testLast', 'Freshman', 3.2, 'A', 'first last, email', 'testattachment')")
+        #Students_test:
+        self.cursor.execute("INSERT INTO Students_test(username, conflict, ranking, sectionRanked, matched) VALUES ('testusername', -1, -1, -1, -1);")
+        #Sections_test:
+        self.cursor.execute("INSERT INTO Sections_test(id, day, time, username, applicant, capacity, numEnrolled) VALUES (3, 'MonWed', '11:30 AM - 12:45 PM', 'testProfusername', 'testStudusername', 50, 2);")
+        self.cursor.execute("INSERT INTO Sections_test(id, day, time, username, applicant, capacity, numEnrolled) VALUES (3, 'second', '11:30 AM - 12:45 PM', 'SECOND', 'SECONDapplicant', 20, 7);")
+        #Professors_test:
+        self.cursor.execute("INSERT INTO Professors_test(username, section, ranking, appRanked) VALUES ('testProfUsername', 3, 1, 'testStudUsername')")
+        #Users_test:
+        self.cursor.execute("INSERT INTO Users_test(username, password, type, firstName, lastName) VALUES ('testUsername', 'testPassword', True, 'testFName', 'testLName')")
+        #Matchings_test:
+        self.cursor.execute("INSERT INTO Matchings_test(section, username) VALUES (3, 'testStudUsername')")
+        self.cursor.execute("INSERT INTO Matchings_test(section, username) VALUES (3, 'testStudUsername2')")   
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 require("dotenv").config()
-const fs = require("fs");
 const express = require('express')
-const path = require("path")
-const multer = require("multer")
 const app = express()
 const port = 3000
+
+const {db, pgp} = require('./database/dbConn')
+
+const fs = require("fs");
+const path = require("path")
+
+
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-fs
+
+const multer = require("multer")
 let storage = multer.diskStorage({
   destination: "./uploads/",
   filename: function(req, file, cb) {
+    //TODO: add username to filename
     cb(null, Date.now() + "-" + file.originalname);
   }
 })
@@ -18,15 +24,11 @@ const upload = multer({ storage: storage })
 
 let AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
-
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
-
-
 
 app.use(cookieParser());
 app.use(session({secret: "TeamSun"}));
 app.use(express.json());
-
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 //Redirect from root to portal pages if the user or logged in, or the login page if not.
@@ -63,7 +65,6 @@ app.get('/logout', function(req, res){
   res.redirect('/login');
 });
 
-
 //To go to and render the applcaiiton submission page
 app.get('/api/redirect/application/submit', (req, res) => {
   res.redirect("/application/submit");
@@ -73,28 +74,41 @@ app.use('/application/submit', (req, res) => {
   res.sendFile(path.join(__dirname, "views", "applicationSubmit.html"));
 });
 
-app.use('/api/application/submit', upload.single('fileAttachment'), (req, res)=>{
-  console.log(req.file);
-  // const key = req.session.user.username + "_" + "attachment.pdf"
+app.use('/api/application/submit', upload.single('fileAttachment'), async (req, res)=>{
   const stream = fs.createReadStream(req.file.destination + req.file.filename);
   const uploadParams = {Bucket: "attachments-429-manager-app", Key: req.file.filename, Body: stream};
 
   link = ""
   // call S3 to retrieve upload file to specified bucket
-  s3.upload(uploadParams, function (err, data) {
-  if (err) {
-      console.log("Upload Failure", err);
-      return res.status(502)
-  } if (data) {
-      console.log("Upload Success", data.Location);
-      link = data.Location;
+  let uploadRes = undefined
+  try{
+    uploadRes = await s3.upload(uploadParams).promise();
+  } catch(err){
+    console.log(err);
+    return res.status(502);
   }
-  });
 
-  const {firstName, lastName, year, reference1_name, reference1_contact, grade,section1_time, section2_time, section3_time, section4_time} = req.body
+  console.log(uploadRes);
+
+  fs.unlink(req.file.destination + req.file.filename, (err) => {
+    if (err) throw err;
+    console.log(req.file.destination + req.file.filename +' was deleted');
+  })
 
   console.log(req.body)
-  //await db.none("INSERT INTO Student(username, conflict, ranking, sectionRanked)")
+  const {name, year, reference1_name, reference1_contact, grade, section1_time, section2_time, section3_time, section4_time} = req.body
+
+  preferences = [[1, parseInt(section1_time)], [2, parseInt(section2_time)], [3, parseInt(section3_time)], [4, parseInt(section4_time)]]
+
+  await db.none("INSERT INTO applicationInfo(username, name, year, grade, referenceName, referenceContact, attachment, preferences, matched)"
+   + "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", 
+   [req.session.user.username, name, year, grade, reference1_name, reference1_contact, uploadRes.Location, preferences, -1])
+  .catch((error)=>{
+    console.log("Application Upload Error");
+    console.log(error);
+    return res.status(404).json("ERROR: soem error occured");
+  })
+  return res.status(200).json("Applcaition submission successful");
 
 });
 
